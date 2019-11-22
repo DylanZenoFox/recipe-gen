91d3983ad1a89d07524f74a01eb7c0ec900dcb43
Dylan Fox <dylfox21@gmail.com>
2019-11-21 17:54:49 -0800

    Title Encoder

--- Encoder-Model/models/title_encoder.py
+++ Encoder-Model/models/title_encoder.py
@@ -5,6 +5,16 @@ import torch.optim as optim
 
 class TitleEncoder(torch.nn.Module):
 
+	# Initialize Model
+	#
+	# Parameters:
+	#
+	# Input: 
+	# 		embedding_dim: dimension of the embedding vector
+	#		hidden_dim: dimension of the GRU hidden dimension
+	#		vocab_size: size of the vocabulary
+	#		bidirectional: Run as bidirectional RNN, Default = False
+
 	def __init__(self, embedding_dim, hidden_dim, vocab_size, bidirectional = False):
 
 		super(TitleEncoder,self).__init__()
@@ -20,46 +30,55 @@ class TitleEncoder(torch.nn.Module):
 
 
 
+
 	# Forward Pass
+	#
 	# Parameters:
+	#
 	# Input: 
 	# 		title: Tensor of shape (batch_size, seq_len) representing a batch of sentences
+	#
 	# Output: 
 	#		output:  Tensor of shape (seq_len, batch_size, hidden_size) representing output for each timestep for each batch
 	#		hidden:  Tensor of shape (num_layers, batch_size, hidden_size) representing the last output of the hidden state
 
 	def forward(self, title):
-		print(title.shape)
-		print(title)
 
+		#Get batch size
 		batch_size = title.size(0)
-		print("batch_size:")
-		print(batch_size)
 
+		#Initialize hidden state
 		hidden = self.initHidden(batch_size)
 
-		print("Hidden:")
-		print(hidden)
-
+		#Get embedding for each word
 		embedded = self.embedding(title)
-		print(embedded)
 		embedded = torch.transpose(embedded,0,1)
 
-		print("Embedded:")
-		print(embedded)
-		print(embedded.shape)
-
+		#Get outputs for each state and hidden state at the end
 		output, hidden = self.gru(embedded,hidden)
 
 		return output, hidden
 
 
 
+	# Initialize Hidden State
+	#
+	# Parameters:
+	#
+	# Input: 
+	# 		batch_size: Minibatch size
+	#
+	# Output: 
+	#		Tensor of shape (num_layers = 1, batch_size, hidden_dim) representing initial hidden state
 
 	def initHidden(self,batch_size):
 
 		return torch.zeros(1, batch_size ,self.hidden_dim)
 
+
+
+
+# TESTING CODE
 
 test = torch.tensor([
 
