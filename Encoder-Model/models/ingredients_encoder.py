241da67a56c3bc94580ca0c461b3c78f2201359d
Dylan Fox <dylfox21@gmail.com>
2019-11-21 18:18:01 -0800

    Adding empty file for ingredients encoder

---
+++ Encoder-Model/models/ingredients_encoder.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class IngredientsEncoder(Torch.nn.module):
+
+	#TODO
+	def __init__():
+
+	#TODO
+	def forward():
+
+	#TODO
+	def initHidden():
+
+
+
+
+class SingleIngredientEncoder(Torch.nn.module):
+
+	#TODO
+	def __init__():
+
+	#TODO
+	def forward():
+
+	#TODO
+	def initHidden():
